b103e0984067001f17618d0196603c0ea4db1a6b
benweddel <benweddel@rogers.com>
2014-12-05 15:03:44 -0500

    functioning mortgage calculator

--- scripts/mrtgApp.js
+++ scripts/mrtgApp.js
@@ -3,15 +3,23 @@ var mrtgApp = {};
 mrtgApp.init = function(){
   
   $('#mtg-calc').on('click', function(){
-    mrtgApp.amt = $('#mtg-amt').val();
-    mrtgApp.intrst = ($('#mtg-int').val())/100;
     mrtgApp.calc();
   });
 };
 
 mrtgApp.calc = function(){
-  console.log(mrtgApp.amt);
-  console.log(mrtgApp.intrst);
+  mrtgApp.amt = $('#mtg-amt').val().replace(/[^0-9.]/g,'');
+  mrtgApp.intrst = $('#mtg-int').val().replace(/[^0-9.]/g,'');
+  mrtgApp.intrst = mrtgApp.intrst / (100 * 12);
+  mrtgApp.period = $('#mtg-period').val().replace(/[^0-9.]/g,'');
+
+  mrtgApp.pmtF = mrtgApp.amt * (mrtgApp.intrst*(Math.pow((1+mrtgApp.intrst),(mrtgApp.period * 12)))) / (Math.pow(1 + mrtgApp.intrst, mrtgApp.period * 12) - 1)
+
+  mrtgApp.pmt = Math.round(mrtgApp.pmtF * 100) / 100;
+  
+  $('#mtg-result').text('Your monthly mortgage payment is '+'$'+mrtgApp.pmt);
+  console.log(mrtgApp.pmt);
+
 };
 
 $(function(){
